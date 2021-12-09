308b89781263fd9943d2bf16070ef4abba109929
aman-raza <amanraza1234@gmail.com>
2021-12-09 19:38:58 +0530

    initial commit

---
+++ src/components/Basics/Resturant.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react'
+import "./style.css"
+import Menu from './menuApi'
+import MenuCard from './MenuCard'
+
+const Resturant = () => {
+    // const [menuData, setMenuData] = useState(initialdata)
+    const [menuData, setMenuData] = useState(Menu)
+
+    return (
+        <>
+            <nav className="navbar">
+                <div className="btn-group">
+                    <button className="btn-group__item">Breakfast</button>
+                </div>
+            </nav>
+
+            <MenuCard menuData={menuData} />
+        </>
+    )
+}
+
+export default Resturant
