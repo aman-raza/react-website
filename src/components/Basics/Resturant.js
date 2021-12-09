90be9a018c93a1bf1addc158fdf1867086570f35
aman-raza <amanraza1234@gmail.com>
2021-12-09 22:00:26 +0530

    final commit

--- src/components/Basics/Resturant.js
+++ src/components/Basics/Resturant.js
@@ -2,19 +2,34 @@ import React, {useState} from 'react'
 import "./style.css"
 import Menu from './menuApi'
 import MenuCard from './MenuCard'
+import NavBar from './NavBar'
+
+const uniqueList = [...new Set(Menu.map((curElem) => {
+    return curElem.category
+})), "All"]
+
+console.log(uniqueList)
 
 const Resturant = () => {
     // const [menuData, setMenuData] = useState(initialdata)
     const [menuData, setMenuData] = useState(Menu)
+    const [menuList, setMenuList] = useState(uniqueList)
+
+    const filterItem = (category) => {
+        if (category === "All") {
+            setMenuData(Menu)
+            return
+        }
+
+        const updatedList = Menu.filter((curElem) => {
+            return curElem.category === category
+        })
+        setMenuData(updatedList)
+    }
 
     return (
         <>
-            <nav className="navbar">
-                <div className="btn-group">
-                    <button className="btn-group__item">Breakfast</button>
-                </div>
-            </nav>
-
+            <NavBar filterItem={filterItem} menuList={menuList}/>
             <MenuCard menuData={menuData} />
         </>
     )
