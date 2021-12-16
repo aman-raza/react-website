2bc024853bfd264f34ae00cd39b8291b56cc7b89
aman-raza <amanraza1234@gmail.com>
2021-12-16 07:12:56 +0530

    hooks2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Resturant from './components/Basics/Resturant'
+import UseState from './components/Hooks/useState'
+
 
 const App = () => {
   return (
     <div>
-      <Resturant />
+      <UseState />
       </div>
   )
 }
--- src/components/Hooks/useState.js
+++ src/components/Hooks/useState.js
@@ -2,23 +2,26 @@ import React from 'react'
 import "./style.css"
 
 const useState = () => {
+    const initialData = 0;
+    const [myNum, setMyNum] = React.useState(initialData);
+
     return (
         <>
             <div className='center_div'>
-                <p>my data</p>
-                <div className='button2'>
+                <p>{ myNum }</p>
+                <div className='button2' onClick={() => setMyNum(myNum + 1)}>
                     <span></span>
                     <span></span>
                     <span></span>
                     <span></span>
-                    HOVER ME
+                    INCR
                 </div>
-                <div className='button2'>
+                <div className='button2' onClick={() => (myNum > 0 ? setMyNum(myNum - 1) : setMyNum(0))}>
                     <span></span>
                     <span></span>
                     <span></span>
                     <span></span>
-                    HOVER ME
+                    DECR
                 </div>
             </div>
         </>
